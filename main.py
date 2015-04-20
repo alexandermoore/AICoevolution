a3d42d58fcdd6a97853467f4657bbd26559773ff
alexandermoore <alexmoore700@gmail.com>
2015-04-19 22:14:43 -0400

    Minor population changes

--- main.py
+++ main.py
@@ -23,15 +23,15 @@ speciesB_map["spB"] = -50
 speciesB_map["fd1"] = 5
 speciesB_map["fd2"] = 5
 
-DIM = 25
+DIM = 10
 # All this is just to maintain the same proportion of each species that you had when the world was 100x100.
 # You had 1500 of each in a 100x100 world, so here I'm just scaling it up/down based on the actual dimension.
-POP_A = int(DIM**2 * (1500.0/100**2)) # 1500
-POP_B = int(DIM**2 * (1500.0/100**2)) # 1500
-NUM_FOOD_1 = int(DIM**2 * (1500.0/100**2)) # 1500
-NUM_FOOD_2 = int(DIM**2 * (1500.0/100**2)) # 1500
+POP_A = 5
+POP_B = 5
+NUM_FOOD_1 = 10
+NUM_FOOD_2 = 10
 STEPS = DIM**2
-NUM_GENERATIONS = 1000
+NUM_GENERATIONS = 2000
 
 def dd():
     return defaultdict(dict)
@@ -39,7 +39,7 @@ def dd():
 if __name__ == '__main__':
 	#p_range = range(-100, 101, 50)
 	#params = itertools.product(p_range, p_range, p_range, p_range, p_range, p_range, p_range, p_range)
-	params = [[0,100,20,0,100,0,0,20]]
+	params = [[-10000,100,2,0,  0,-10000,0,200]]
 	data_file_name = "data"
 	now = str(datetime.now())
 	file_no = 0
@@ -51,9 +51,9 @@ if __name__ == '__main__':
 		if not os.path.exists(data_file_name + "/" + now):
 			os.makedirs(data_file_name + "/" + now)
 	else:
-		now_win = now.replace(":","-")
-		if not os.path.exists(data_file_name + "/" + now_win):
-			os.makedirs(data_file_name + "/" + now_win)
+		now = now.replace(":","-")
+		if not os.path.exists(data_file_name + "/" + now):
+			os.makedirs(data_file_name + "/" + now)
 
 	for param in params:
 		print(param)
@@ -108,35 +108,7 @@ if __name__ == '__main__':
 
 			genomesA = evolverA.evolve(fitnessA)
 			genomesB = evolverB.evolve(fitnessB)
-			fit_countB = [(0.0,0.0)] * len(genomesB)
-
-			for i in range(len(genomesA)):
-				networkA = genomesA[i]
-				for j in range(len(genomesB)):
-					networkB = genomesB[j]
-
-					specificationA = (POP_A, networkA)
-					specificationB = (POP_B, networkB)
-					fitA, fitB, stats = arena.generate(DIM, specificationA, specificationB, NUM_FOOD_1, NUM_FOOD_2, STEPS)
-
-					(old_fit, old_count) = fit_countA[i]
-					fit_countA[i] = (old_fit + fitA, old_count + 1.0)
-
-					(old_fit, old_count) = fit_countB[j]
-					fit_countB[j] = (old_fit + fitB, old_count + 1.0)
-
-			fitnessA = []
-			fitnessB = []
-
-			for i in range(len(genomesA)):
-				fitnessA.append((genomesA[i], fit_countA[i][0]/fit_countA[i][1]))
-
-			for i in range(len(genomesB)):
-				fitnessB.append((genomesB[i], fit_countB[i][0]/fit_countB[i][1]))
-
-			genomesA = evolverA.evolve(fitnessA)
-			genomesB = evolverB.evolve(fitnessB)
-
 
 		pickle.dump({"A" : speciesA_map, "B" : speciesB_map, "stat": stats_record}, open(data_file_name + "/" + now + "/" + str(file_no), "wb"))
+		print(stats_record[NUM_GENERATIONS-1])
 		file_no += 1
