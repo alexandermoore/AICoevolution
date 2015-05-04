5b83344a566ae3a5fbd18de18e9d5b9e27e8be11
Armaghan Behlum <behlum@college.harvard.edu>
2015-05-04 15:34:59 -0400

    Recover util.py

--- util.py
+++ util.py
@@ -18,6 +18,22 @@ GLOBAL_PARAMS = {
 	"A_far_and_fast_B_far": {
 		SPECIES_A: {"sight_range": 4, "move_speed": 2},
 		SPECIES_B: {"sight_range": 4, "move_speed": 1}
+	},
+	"A_fast_B_fast": {
+		SPECIES_A: {"sight_range": 2, "move_speed": 2},
+		SPECIES_B: {"sight_range": 2, "move_speed": 2}
+	},
+	"A_far": {
+		SPECIES_A: {"sight_range": 4, "move_speed": 1},
+		SPECIES_B: {"sight_range": 2, "move_speed": 1}
+	},
+	"A_fast": {
+		SPECIES_A: {"sight_range": 2, "move_speed": 2},
+		SPECIES_B: {"sight_range": 2, "move_speed": 1}
+	},
+	"B_fast": {
+		SPECIES_A: {"sight_range": 2, "move_speed": 1},
+		SPECIES_B: {"sight_range": 2, "move_speed": 2}
 	}
 }
 
